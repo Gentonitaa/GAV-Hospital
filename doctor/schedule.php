0a40c4b6309ad63b9194bcb0e8e53b0c985fa9d8
Pranvera Berisha <pranveraberisha26@hotmail.com>
2023-05-04 18:35:16 +0200

    Update schedule.php

--- doctor/schedule.php
+++ doctor/schedule.php
@@ -436,7 +436,7 @@
                                              <td colspan="7">
                                              <br><br><br><br>
                                              <center>
-                                             <img src="../img/notfound.svg" width="25%">
+                                             
                                              
                                              <br>
                                              <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
